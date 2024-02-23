3f20f971723807ff63ac87096262f00224b02636
Logan Bunch <loganbunch@gmail.com>
2024-02-22 22:17:27 -0800

    added page not found for wildcard paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./layout.jsx";
 import Home from "./pages/Home.jsx";
 import Maps from "./pages/Maps.jsx";
 import Records from "./pages/Records.jsx";
+import NotFound from "./pages/NotFound.jsx";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Layout>
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="/maps" element={<Maps />} />
           <Route path="/records" element={<Records />} />
