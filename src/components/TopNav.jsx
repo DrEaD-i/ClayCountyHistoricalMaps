d999349025c807f736bc5a6bb3e0c20ab1cbbd94
Logan Bunch <loganbunch@gmail.com>
2024-02-23 20:02:20 -0800

    css adjustments

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,29 +1,38 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import './TopNav.css'
 
 export default function TopNav() {
   const [active, setActive] = useState(window.location.pathname);
 
   return (
     <nav className="topnav">
-      <Link
-        className={active === "/" ? "active" : null}
-        onClick={() => setActive("/")}
-        to="/">
-        Home
-      </Link>
-      <Link
-        className={active === "/maps" ? "active" : null}
-        onClick={() => setActive("/maps")}
-        to="/maps">
-        Maps
-      </Link>
-      <Link
-        className={active === "/records" ? "active" : null}
-        onClick={() => setActive("/records")}
-        to="/records">
-        Records
-      </Link>
+      <ul className="nav-list">
+        <li>
+          <Link
+            className={active === "/" ? "active" : null}
+            onClick={() => setActive("/")}
+            to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={active === "/maps" ? "active" : null}
+            onClick={() => setActive("/maps")}
+            to="/maps">
+            Maps
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={active === "/records" ? "active" : null}
+            onClick={() => setActive("/records")}
+            to="/records">
+            Records
+          </Link>
+        </li>
+      </ul>
       <input type="text" placeholder="Search.." />
     </nav>
   );
