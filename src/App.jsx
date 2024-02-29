484c9622707c151ede02175064301eb15244a118
Logan Bunch <loganbunch@gmail.com>
2024-02-28 23:27:30 -0800

    fixed routes, maybe

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from "react-router-dom";
 import Layout from "./layout.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Maps from "./pages/Maps.jsx";
 import Records from "./pages/Records.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import "./App.css";
 
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Home />, errorElemcnt: <NotFound /> },
+    { path: "/maps", element: <Maps /> },
+    { path: "/records", element: <Records /> },
+  ],
+  { basename: "/ClayCountyHistoricalMaps" }
+);
+
 function App() {
   return (
     <>
       <Layout>
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/maps" element={<Maps />} />
-          <Route path="/records" element={<Records />} />
-        </Routes>
+        <RouterProvider router={router} />
       </Layout>
     </>
   );
