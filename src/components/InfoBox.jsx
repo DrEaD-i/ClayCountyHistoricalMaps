aaf18b8a5455788e5b69bfa25755463411c5fb0b
Logan Bunch <loganbunch@gmail.com>
2024-04-30 18:18:18 -0700

    reverting to previous commit

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,10 +1,8 @@
-import "./InfoBoxDesign.css/";
 export default function InfoBox({ city }) {
 
   return (
-    <div className="InfoBoxDesign text-white bg-red-900 rounded-md p-4 m-4 min-w-60 text-align: center">
+    <div className="text-white bg-red-400 rounded-md p-4 m-4 min-w-60">
       {city}
     </div>
-    
   )
 }
--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,30 +1,7 @@
 import Layout from "../layout.jsx";
-<<<<<<< Updated upstream
 import MapCanvas from "../components/MapCanvas.jsx";
-=======
-import {useRef,useEffect} from 'react';
->>>>>>> Stashed changes
 
 export default function Map() {
-   const canvasRef = useRef()
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-
-  useEffect(() => {
- 
-    canvas.width= window.innerHeight;
-    canvas.height= window.innerWidth;
-    
-    const mapImage= new Image();
-    mapImage.src=ClayCountyMap;
-    
-    Image.onload= function(){
-        ctx.drawImage(Image, window.innerWidth, window.innerHeight);
-    }
-    
-
-  }, []);
-
   return (
     <Layout>
       <MapCanvas />
